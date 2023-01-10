2c0d4bac0f7dd1659bbcc5c7c263c9d702399e81
babyyod4 <razzlenator@protonmail.com>
2023-01-10 18:09:20 +0000

    added example endnode-tranceiver

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Endnode Receiver
+# Endnode Tranceiver
 
-In this test we try to receive an 5byte data packet consisting of a 6byte header. 
+In this test we use a single rfm95w as a tranceiver. It has two states, RX_MODE and TX_MODE. The tranceiver first starts in RX mode where i received data from a gateway (see test `gateway-tranceiver`). After receiving data 10 times, it goes into TX mode where it send data for ever to a gateway. 
 
+This is to test if the switch from TX and RX is fluid. 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "libsoilar/drivers/rfm95w/rfm95w.h"            
+#include "soilar-module-template/drivers/rfm95w/rfm95w.h"             
 #include INIT_ENVOIRMENT
 
-// Uno Receiver
+// Uno Tranceiver
 /*
   DIO0 - D2
   RESET - D8
@@ -11,7 +11,7 @@
   MISO - D12
 */
 
-// Due Receiver
+// Due Tranceiver
 /*
   DIO0 - D22
   RESET - D12
@@ -23,8 +23,9 @@
 
 
 
-LoraEndnode receiver;
+LoraEndnode transceiver;
 
+// TX settings
 LoraEndnodeSettings settings = { 
   .frequencyMHz = RFM95W_CALC_FEQ_FAST( ((long)(867E6) - 125000) ),  
   .signalBandwidthHz = RFM95W_BW_250KHZ,
@@ -38,32 +39,51 @@ LoraEndnodeSettings settings = {
   .invertIQ = false
 };
 
+// RX settings
+LoraEndnodeSettings settings2 = { 
+  .frequencyMHz = RFM95W_CHANNEL_868_1,  
+  .signalBandwidthHz = RFM95W_BW_125KHZ,
+  .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+  .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+  .spreadingFactor = RFM95W_DEFAULT_SF,
+  .codeRate = RFM95W_CODE_RATE_4_6,
+  .syncWord = 0x12,
+  .enableCRC = true,
+  .lowPowerReceiveMode = false,
+  .invertIQ = false
+};
+
 Rfm95wInterface interface = { 0, 9, 8, 2 };
 
-uint8_t buff[8] = {0};
+uint8_t buff[11] = {0}; // RX BUFF
+uint8_t buff2[8] = {0xFF, 0x01, 0x00, 0x00, 0x0D, 0xDD, 0xFF, 0xFF}; // TX BUFF
+
+
+uint8_t change = 0;
+uint8_t i=0;
+bool txMode = true;
 
 ISR_CODE void received(void);
 
 void received(void){
 
   // received a packet
-  Serial.print("Device [1] -- Received packet ");
-  if ( receiver.read(buff, 8) == LEC_PKG_CORRUPTED ){
+  print("Received packet '");
+  if ( transceiver.read(buff, 11) == LORA_PKG_CORRUPTED ){
     Serial.println("Corrupted");
   } else{
-    Serial.print("[");
-    Serial.print(buff[3]); 
-    Serial.print("] ");
- 
+    Serial.print("Received packet '");
     Serial.print(buff[4]); 
-    Serial.print(" ");
+    Serial.print("-");
     Serial.print(buff[5]); 
-    Serial.print(" ");
-  
-    Serial.print("-- From device [");
-    Serial.print(buff[1]);
-    Serial.print("]");
-    Serial.print("\r\n");   
+    Serial.print("-"); 
+    Serial.print(buff[6]); 
+    Serial.print("-"); 
+    Serial.print(buff[7]); 
+    Serial.print("-");
+    Serial.print(buff[8]); 
+    Serial.print("'\r\n");   
+    change++;
   }
   
 }
@@ -73,15 +93,37 @@ void setup(){
   Serial.begin(9600);
   Serial.println("LoRa Receiver");
 
-  if ( CreateRfm95W(&receiver, &interface, &settings, 0) == LEC_ERROR_WRONG_DEVICE ) {
+
+  if ( CreateRfm95W(&transceiver, &interface, &settings2) == LORA_ERROR_WRONG_DEVICE ) {
     Serial.println("Starting LoRa failed!");
     while(1);
   }
 
-  receiver.addOnReceiveCallback( received );
-  receiver.setMode( LEM_RX_LOW_POWER_MODE );
-  SelectLoraEndnodeDevice(0);
+  transceiver.setOnReceiveCallback( received );
 
 }
 
-void loop(){}
+void loop(){
+  
+  if (txMode){
+    if(i > 250){ i=0; }
+    buff2[3]=i;
+    transceiver.send(buff2, 8);
+    Serial.print("Device [1] -- Sending packet [");
+    Serial.print(buff2[3]); 
+    Serial.print("] "); 
+    Serial.print(buff2[4]); Serial.print(" "); Serial.print(buff2[5]);
+    Serial.print("\r\n");
+    i++;
+    delay(5000);
+  }
+
+  if (change >= 10){
+    transceiver.setMode( LORA_TX_MODE );
+    transceiver.hop( &settings2 );
+    txMode = true;
+    change = 0;
+    delay(20000);
+  }
+  
+}
